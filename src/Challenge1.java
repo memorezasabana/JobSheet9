fb0329b635b8d3ccbc6ee6abd55e4cd03d0cb4ef
memorezasabana <memoreza_sabana_28rpl@student.smktelkom-mlg.sch.id>
2019-09-12 10:49:13 +0700

    first commit

---
+++ src/Challenge1.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author TUF
+ */
+public class Challenge1 {
+     public static void main(String[] args) {
+String[]nama= {"Rendi Prayata Wibawa","Natasya Adelia Ramadhanti","Ruqul Adam Himawan","Viko Achmat Vernanda","Rensi Meila Yuvinata","Naufal Gajendra Ardiputra","Tembang Raras Kusumawuri","Diva Maretta","Fatan Virgian Nova","Satmika Antargata Ozora"};
+String[]alamat={"Tuban","Blitar","Blitar","Probolinggo","Kediri","Malang","Ngawi","Malang","Madiun","Banyuwangi"};
+String[] nohp= {"085804146990","085646302778","0859193774917","081217507779","082334929755","0895083640087","088235133444","0812525552982","082241831307","082232175005"};
+String[]hobi= {"Bermain Musik","Membaca Novel","Editing","Gaming","Stalking","Renang","Makan","Ghibah","Basket","Voli"};
+String[]akunig= {"rendiwibawa17","ntsyade","adam.hmwan","captain himer","rensimyl","naufalardiputra","trarask","diva_marettaa","fatan_nova","antarozora"};
+         System.out.println("==========B I O D A T A   S I S W A==========");
+         System.out.println("NAMA LENGKAP   : "+ nama[4]);
+         System.out.println("ALAMAT         : Kabupaten/Kota* "+ alamat[4]);
+         System.out.println("NO HP          : "+ nohp[4]);
+         System.out.println("HOBI           : "+ hobi[4]);
+         System.out.println("AKUN INSTAGRAM : "+ akunig[4]);
+         System.out.println("=============================================");
+         System.out.println("* Pilih salah satu");
+}
+}
+
+
+     
---
+++ src/jobsheet9/Array.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jobsheet9;
+import java.util.Scanner;
+/**
+ *
+ * @author TUF
+ */
+public class Array {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       String[]nama = {"Yulius","Risa","Sabrina","Naisya","Moza","Rendi","Rensi","Diva","Fara"};
+        System.out.print(" Nama teman pertama saya adalah "+ nama[1]+ " ----- "+ " kedua : " + nama[2] + " ----- "+" ketiga : "+ nama[3]);
+        
+    
+}
+     
+    }
+    
+
